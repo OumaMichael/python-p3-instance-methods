0d84caee3f2ad933415e823addc8301fa6d3fd67
Michael Ouma <Oumamichael108@gmail.com>
2025-05-10 13:23:11 +0300

    Lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,15 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
     pass
+Max = Dog()
+Max.bark()
+Max.sit()
+Rex = Dog()
+Rex.bark()
+Rex.sit()
